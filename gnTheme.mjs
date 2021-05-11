2d716356999eeb4b27877387e1385caf2d36f9c8
Oj <oj@oojmed.com>
2021-05-11 09:56:11 +0100

    [Chore] Bump version, tweak desc

--- gnTheme.mjs
+++ gnTheme.mjs
@@ -1,5 +1,5 @@
 export default {
   name: 'Revert Rebrand',
-  description: 'Reverts Discord\'s 2021 rebrand (font, colors, reactions, mentions, loading screen, home icon)',
-  version: '2.0'
+  description: 'Reverts Discord\'s 2021 rebrand (font, colors, reactions, mentions, loading screen, home icon, reply ping color)',
+  version: '2.1'
 };
