20a87bc43d50f6d9ce4c12b3ad6f441dc99a5812
Oj <oj@oojmed.com>
2021-05-27 17:26:30 +0100

    [Chore] Bump version in GT manifest

--- gnTheme.mjs
+++ gnTheme.mjs
@@ -1,7 +1,7 @@
 export default {
   name: 'Revert Rebrand',
   description: 'Reverts Discord\'s 2021 rebrand (font, colors, reactions, mentions, loading screen, home icon, reply ping color, and more)',
-  version: '3.3',
+  version: '4.0',
   screenshots: [
     ['Original (Without Theme)', 'https://raw.githubusercontent.com/Goose-Nest/GT-RevertRebrand/main/screenshots/message_original.png'],
     ['With Revert Rebrand', 'https://raw.githubusercontent.com/Goose-Nest/GT-RevertRebrand/main/screenshots/message_revert.png']
