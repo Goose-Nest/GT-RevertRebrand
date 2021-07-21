caaa0d9abbd7a6e09b4c240d5d4e7642e3bc04a7
Oj <oj@oojmed.com>
2021-07-21 11:43:50 +0100

    [Hardcoded] Add

--- gnTheme.mjs
+++
@@ -1,9 +0,0 @@
-export default {
-  name: 'Revert Rebrand',
-  description: 'Reverts Discord\'s 2021 rebrand (font, colors, reactions, mentions, loading screen, home icon, reply ping color, and more)',
-  version: '4.2',
-  screenshots: [
-    ['Original (Without Theme)', 'https://raw.githubusercontent.com/Goose-Nest/GT-RevertRebrand/main/screenshots/message_original.png'],
-    ['With Revert Rebrand', 'https://raw.githubusercontent.com/Goose-Nest/GT-RevertRebrand/main/screenshots/message_revert.png']
-  ]
-};
